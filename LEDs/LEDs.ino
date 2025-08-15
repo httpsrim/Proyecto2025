d6a9497b029f53765aa3da0be38d859163d1c289
httpsrim <ines.jimenez2003@gmail.com>
2025-08-15 13:24:57 +0200

    V2, all the components are included here

---
+++ LEDs/LEDs.ino
@@ -0,0 +1,23 @@
+  #include <Adafruit_NeoPixel.h>
+  #define PIN   A3
+  #define NUMPIXELS 29
+
+  using namespace std;
+
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+bool primeraVez = true;
+
+void setup() {
+  Serial.begin(9600);
+  pixels.begin();
+  for(int i = 0; i < NUMPIXELS; i++){
+    int colorR = random(0,250);
+    int colorB = random(0,250);
+    int colorG = random(0,250);
+    pixels.setPixelColor(i, pixels.Color(colorR, colorG, colorB));
+    pixels.show();  
+  }
+}
+
+void loop() {
+}
